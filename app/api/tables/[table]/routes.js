f4e58629b3804266aa222e73a2c1336e86f9cdd5
Sohan-Kamble <129823388+Sohan-Kamble@users.noreply.github.com>
2024-12-16 11:24:57 +0530

    Update routes.js

--- app/api/tables/[table]/routes.js
+++ app/api/tables/[table]/routes.js
@@ -15,10 +15,15 @@ export default async function handler(req, res) {
   try {
     console.log('Attempting to establish database connection...');
     const connection = await mysql.createConnection({
-      host: '144.13.1.11',
-      user: 'singssd',
-      password: 'singssd@123',
-      database: 'sing_ssd',
+      // host: '144.13.1.11',
+      // user: 'singssd',
+      // password: 'singssd@123',
+      // database: 'sing_ssd',
+
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME;
 
     });
 
